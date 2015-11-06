bebd215d680e11807eded1c333249b5c87157fc6
wp <wp@yitong.com.cn>
2015-11-06 10:00:35 +0800

    修改模板工程结构

---
+++ views/index/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+
+require.async(function(){
+
+    var _app = {
+        init : function(){
+            document.body.innerHTML = '<h1>Hello world!</h1>';
+        }
+    }
+
+    _app.init();
+});
+
